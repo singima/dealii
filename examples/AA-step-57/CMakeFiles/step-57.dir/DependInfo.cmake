1dcee126a0d8718e3763f3840cc1879d785a5229
Sean Ingimarson <singima@clemson.edu>
2022-09-13 09:24:42 -0700

    9/13 commit

---
+++ examples/AA-step-57/CMakeFiles/step-57.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/seanwalkeri95/deal.II/dealii/examples/step-57/step-57.cc" "/home/seanwalkeri95/deal.II/dealii/examples/step-57/CMakeFiles/step-57.dir/step-57.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_NO_AUTO_PTR"
+  "DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/lib/x86_64-linux-gnu/openmpi/include/openmpi"
+  "/usr/lib/x86_64-linux-gnu/openmpi/include"
+  "/usr/include/trilinos"
+  "/usr/include/hdf5/openmpi"
+  "/usr/include/scotch"
+  "/usr/include/suitesparse"
+  "/usr/include/petsc"
+  "/usr/lib/x86_64-linux-gnu/hdf5/openmpi/include"
+  "/usr/include/opencascade"
+  "/usr/include/slepc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ examples/AA-step-57/CMakeFiles/step-57.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/step-57.dir/step-57.cc.o"
+  "step-57"
+  "step-57.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/step-57.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
